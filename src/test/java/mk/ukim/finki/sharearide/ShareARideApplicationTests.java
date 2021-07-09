1611ca9465844ca23f10052d26421f30a592df18
Aleksandar Ivanovski <aleksandar.ivanovski123@gmail.com>
2021-07-09 20:09:40 +0200

    Initial commit

---
+++ src/test/java/mk/ukim/finki/sharearide/ShareARideApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.sharearide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareARideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
